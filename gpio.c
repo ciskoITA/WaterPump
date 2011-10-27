f2faa28b6e5500527529e9217ae36d3a570724a2
Francesco Sarti <francesco.sarti01@ateneopv.it>
2011-10-27 17:25:01 +0200

    start configuring gpio

---
+++ gpio.c
@@ -0,0 +1,24 @@
+#include "gpio.h"
+#include "io.h"
+
+void gpio_setup(void)
+{
+        //accendo il clock per poter configurare correttamente i pin di trasmissione e di ricezione
+        *AHBCLKCTRL |= 0x010000;
+
+        //configurazione pin UART
+        *GPIO1_6 |= 0x01;
+        *GPIO1_7 |= 0x01;
+        
+        //configurazione pin I2C
+        //configurazione su UEXT (SCL)
+        *GPIO0_4 |= 0x01;
+        //configurazione su UEXT (SDA)
+        *GPIO0_5 |= 0x01;
+        
+        //pin out per i2c
+        *GPIO0DIR |= 0x30;
+        
+        //accensione delle porte GPIO (devo impostare il bit 6 di AHBCLKCTRL)
+        *AHBCLKCTRL |= 0x40;
+}
