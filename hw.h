69aaa3fe36afb964e92366e65645d48cf1c3859e
Francesco Sarti <francesco.sarti01@ateneopv.it>
2011-11-04 18:04:46 +0100

    gpio config update

--- hw.h
+++ hw.h
@@ -40,20 +40,43 @@ extern volatile uint32_t regs[];
 /* GPIO PORT 1 --> water pump read level
 4bits used:
 GPIO1_0 : GPIO1_2 the 3 bits used to read the level from 74148,
-GPIO1_3 is NOT[NOT(GS)*EO] from 74148 to detect overflow --> I have 8 standard level (values 0 to 7) plus another for warning function */
+GPIO1_3 is NOT[NOT(GS)&EO] from 74148 to detect overflow --> I have 8 standard level (values 0 to 7) plus another for warning function */
 #define REG_GPIO1DAT		(0x50013ffc / 4)
 #define REG_GPIO1DIR		(0x50018000 / 4)
-
-//for DIR 0=read, 1=write
-//to read a single bit use PIO1_n
-#define PIO1_0			(0x50013ffc / 4)
-#define PIO1_1			(0x50013ffd / 4)
-#define PIO1_2			(0x50013ffe / 4)
-#define PIO1_3			(0x50013fff / 4)
+//to read values of water pump
+//using the DATAMASK in GPIO1
+#define LEVEL			(0x5001001c / 4) //read values from 0 to 7
+#define LEVEL_MASK		0x7
+//#define WARNING		(0x50010010 / 4
+#define WARNING			(0x50010020 / 4) //read value 8 if WARNING==true
+#define WARNING_MASK		0x8
+//usage levelread=regs[LEVEL]&LEVEL_MASK
 
 /* GPIO PORT 1 --> water pump output funtion
 2 functions:
 GPIO1_4 for pump on/off,
-GPIO1_5 for buzzer */
-#define PIO1_4			(0x50014000 / 4)
-#define PIO1_5			(0x50014001 / 4)
+GPIO1_5 for buzzer
+using DATAMASK */
+#define POWER			(0x50010040 / 4) //to set pump on/off
+//#define POWER_ON		15
+#define POWER_ON		0x10
+//#define POWER_OFF		0
+#define POWER_OFF		0x0
+#define BUZZER			(0x50010080 / 4) //to set buzzer on/off
+//#define BUZZER_ON		32
+#define BUZZER_ON		0x20
+//#define BUZZER_OFF		0
+#define BUZZER_OFF		0x0
+//usage: regs[POWER]=POWER_ON or POWER_OFF --> pump on/off
+//usage: regs[BUZZER]=BUZZER_ON or BUZZER_OFF --> buzzer on/off
+//usage regs[POWER]&POWER_ON or POWER_OFF --> pump on/oof
+
+//writing op: bits at 1 into the register select bit to be written. AND operation with the register write bits
+
+//if DIR 0=read, else 1=write
+//function dir setting: 0,1,2,3 input, 4,5 ouptut 0000 0018 in hex
+//#define SET_GPIO1DIR		18
+#define SET_GPIO1DIR		0x30
+//to set use regs[REG_GPIO1DIR]=SET_GPIO1DIR
+
+
--- task-pump.c
+++ task-pump.c
@@ -1,21 +1,72 @@
 #include "thos.h"
 #include "hw.h"
 
+static int pump_status=0; //0 pump is off, 1 pump is on
+
 static int pump_init(void *unused)
 {
-	//regs[REG_IODIR] |= 7 << 19; /* gpio 19, 20, 21 */
-	//return 0;
+	//initialize hw
+	//setting the IO pin
+	regs[REG_GPIO1DIR]=SET_GPIO1DIR;
+	//at startup PUMP & BUZZER off
+	regs[POWER]=POWER_OFF;
+	regs[BUZZER]=BUZZER_OFF;
 	
-	//setto i GPIO come pin di lettura
+	pump_status=0;
+	return 0;
+	//next version read first level when initialize
 }
 
 static void *pump(void *arg)
 {
-	/*int i = (int)arg & 7;
+	//read value
+	value=regs[LEVEL];
+	//or value=(regs[LEVEL])&LEVEL_MASK;
+	
+	//values from 0 to 4 are ok, if 5 or greather pump on, 7 buzzer on
+	//when pump is on, turn it off only when level is 2 to avoid damage due to multiple on/off
+	/*switch(value) {
+		case 0 {
+			regs[POWER]=POWER_OFF;
+			pump_status=0;
+		}
+		case 1 {WARNI
+			regs[POWER]=POWER_OFF;
+			pump_status=0;
+		}
+		case 2 {
+			regs[POWER]=POWER_OFF;
+			pump_status=0;
+		}
+		case 3 {
+			if (pump_status=1) {
+				regs[POWER]=POWER_ON;
+			pump_status=1;
+			}
+		}
+	}*/
+	if(value>4) {
+		regs[POWER]=POWER_ON;
+		pump_status=1;
+		if(value==7) {
+			regs[BUZZER]=BUZZER_ON;
+			if (WARNING==8){
+			//ok, warning but do nothing for now			
+			}
+		}//end value 7
+		regs[BUZZER]=BUZZER_OFF;
+	}//end if
+	//else value<=4; if <=2 pump off
+	else if (value<=2) {
+		regs[POWER]=POWER_OFF;
+		pump_status=0;
+	}
+	//else value is 3 or 4, if the pump is on leave it on, else the pump is off and leave it off
+	else {
+	}
+	
 
-	regs[REG_IOSET] = i << 19;
-	regs[REG_IOCLR] = (i^7) << 19;
-	return (void *)++i;*/
+	//next version returns the value just read to the next cicle
 }
 
 static struct thos_task __task t_led = {
